3b3532ea6bcb0962155e008296e09fda2b7de402
Ndey Isatou Jobe <isa2jobe@gmail.com>
2018-03-21 12:44:51 +0100

    Add sms script

---
+++ send_text.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACa71f866c339eb319a58b03c8bb0cb944"
+# Your Auth Token from twilio.com/console
+auth_token  = "6930b64e1e47ad4551fb15643a562293"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+46769140194",
+    from_="+46765193294",
+    body="Good luck at the embassy! Please come back to look at my code in the afternoon. This is an automated message.")
+
+print(message.sid)
